dd5a506b8dc8f23df4bf0c6d8005fb1cbe811ee4
AM <anmuntean@pentalog.com>
2017-11-01 15:20:28 +0200

    Template inheritance, posts mock.

--- app/views.py
+++ app/views.py
@@ -5,4 +5,18 @@ from app import app
 @app.route('/index')
 def indexAction():
     user = {'nickname': 'Slab'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'nickname': 'Dr. Zed'},
+            'body': 'You know leaves a hole of a bullet but a\'int a bullet? Cause I freakin\' don\'t.'
+        },
+        {
+            'author': {'nickname': 'Brick'},
+            'body': 'I punch her problems!!'
+        },
+        {
+            'author': {'nickname': 'Mr. Torgue'},
+            'body': 'EXPLOSIONS??!!!!'
+        }
+    ]
+    return render_template('index.html.jinja2', title='Home', user=user, posts=posts)
