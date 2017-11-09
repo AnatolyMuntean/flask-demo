5cc29862b2f7cafdbcbc5d76f21ee201aeb965c1
AM <anmuntean@pentalog.com>
2017-11-09 14:33:09 +0200

    Follower async notifications.

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,13 @@ import os
 from flask_login import LoginManager
 from flask_openid import OpenID
 from config import basedir
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config.from_object('config')
-db = SQLAlchemy(app)
 
+mail = Mail(app)
+db = SQLAlchemy(app)
 lm = LoginManager()
 lm.init_app(app)
 lm.login_view = 'login'
